971b3e6e315ee4784168a7e5dfff38dc946b0fc3
mottafoka <guiap.mota@hotmail.com>
2019-11-28 01:05:37 -0300

    Adaptação da câmera do QRCode

--- RemoteBash/app/src/main/java/com/remotebash/QRCodeCam.kt
+++ RemoteBash/app/src/main/java/com/remotebash/QRCodeCam.kt
@@ -22,7 +22,7 @@ class QRCodeCam : AppCompatActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_qrcode)
         askCameraPermission()
         lastResultVerification()
     }
